83dc452d67f28762c7fc516c36aeee86b1c1690e
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 14:05:17 -0300

    Uri 1009 Salario com Bonus

---
+++ SalarioComBonus/SalarioComBonus/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalarioComBonus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Nome;
+            double SalarioFixo, TotalVenda, SalarioTotal;
+
+            Nome = Console.ReadLine();
+            SalarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            TotalVenda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            SalarioTotal = TotalVenda * 15.0 / 100 + SalarioFixo;
+
+            Console.WriteLine("TOTAL = R$ " + SalarioTotal.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
